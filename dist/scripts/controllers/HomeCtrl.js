03190c75cb7bf983e8b8f35de9c6b81472ae10a6
Kevin Voshell <kevinvoshell@gmail.com>
2017-02-16 01:43:48 -0600

    user story 3 complete

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,15 @@
 (function() {
-    function HomeCtrl(Room) {
+    function HomeCtrl(Room, $uibModal) {
         this.rooms = Room.all;
+        this.open = function() {
+            var modalInstance = $uibModal.open({
+                templateUrl: '/templates/modal.html',
+                controller: 'ModalCtrl as modal',
+            });
+        }
     }
     
     angular 
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
 })();
