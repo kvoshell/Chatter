f8ca1741685bcc06f2a7ceafe13d624debdab5b9
Kevin Voshell <kevinvoshell@gmail.com>
2017-02-21 01:42:30 -0600

    user story 4 complete

---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,21 @@
+(function() {
+    function ModalCtrl(Room, $uibModalInstance) {
+        
+        // Assign local scope for reference in templates
+        var modal = this;
+        
+        // Dismiss modal with new room added to Database/Home Page
+        modal.open = function() {
+            Room.addRoom(modal.room.name);
+            $uibModalInstance.close();
+        }
+        // Dismiss modal without any changes
+        modal.cancel = function() {
+            $uibModalInstance.dismiss();
+        }  
+    }    
+ 
+    angular
+    .module('blocChat')
+    .controller('ModalCtrl', ['Room', '$uibModalInstance', ModalCtrl]);
+})();
